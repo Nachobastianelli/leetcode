f70e226f34a5f2f7c945f70615e903092a7ca6cb
Ignacio Bastianelli <nachobastianelli2003@gmail.com>
2025-07-21 09:03:51 -0300

    Subo la resolucion de MyAtoi

--- 8. String to Integer (atoi)/respuesta.ts
+++ 8. String to Integer (atoi)/respuesta.ts
@@ -1,5 +1,38 @@
-function myAtoi(s: string): number {
-    const INT_MIN = -2 ** 31;
-    const INT_MAX = 2 ** 31 - 1;
+const numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 
-};
+function myAtoi(s: string): number {
+  const INT_MIN = Math.pow(-2, 31);
+  const INT_MAX = Math.pow(2, 31) - 1;
+  let isNegative = false;
+  let newInt: number | string = "";
+  s = s.trimStart();
+
+  if (s[0] === "-") {
+    isNegative = true;
+    s = s.slice(1);
+  } else if (s[0] === "+") {
+    isNegative = false;
+    s = s.slice(1);
+  }
+
+  for (let c of s) {
+    if (numbers.includes(c)) {
+      newInt += c;
+      if (newInt[0] === "0") newInt = "";
+    } else break;
+  }
+
+  newInt = parseInt(newInt);
+
+  if (isNaN(newInt)) return 0;
+
+  if (isNegative) newInt = -newInt;
+  if (newInt > INT_MAX) newInt = INT_MAX;
+  if (newInt < INT_MIN) newInt = INT_MIN;
+
+  return newInt;
+}
+
+const str = "-91283472332";
+
+console.log(myAtoi(str));
